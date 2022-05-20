556a20fc78e161835edc6276b3eac4a73ca5cecc
OldKrab <4shushakov@gmail.com>
2022-05-20 12:04:28 +0700

    add XmlSourceParser and DocumentReader declaration

---
+++ ThematicAnalysis/src/DocumentReader.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Lemmatizer.h"
+
+class DocumentReader
+{
+public:
+	std::vector<NormalizedArticle> read(std::string filePath);
+private:
+	Lemmatizer _lemmatizer;
+};
---
+++ ThematicAnalysis/src/XmlSourceParser.cpp
@@ -0,0 +1,1 @@
+﻿#include "XmlSourceParser.h"
