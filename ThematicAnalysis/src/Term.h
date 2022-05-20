05c80b5894194561906078fbc76859a4e0178e7e
OldKrab <4shushakov@gmail.com>
2022-05-20 15:53:03 +0700

    add constructor to Term

--- ThematicAnalysis/src/Term.h
+++ ThematicAnalysis/src/Term.h
@@ -8,6 +8,8 @@ public:
 	std::vector<std::wstring> words;
 	std::wstring view;
 	double weight;
+
+	Term(std::vector<std::wstring> words, std::wstring view);
 	size_t getHashCode();
 private:
 	size_t _hashCode;
