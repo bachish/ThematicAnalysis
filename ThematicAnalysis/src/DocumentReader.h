0bdd26232ebef0e16f11357f635adbf8daa0beba
OldKrab <4shushakov@gmail.com>
2022-05-23 10:17:18 +0700

    add Hasher and DocumentReader defination

--- ThematicAnalysis/src/DocumentReader.h
+++ ThematicAnalysis/src/DocumentReader.h
@@ -1,15 +1,19 @@
 ﻿#pragma once
 #include "Lemmatizer.h"
 #include "NormalizedArticle.h"
+#include "Normalizer.h"
+#include "XmlSourceParser.h"
 
 class DocumentReader
 {
 public:
 	// throw std::ifstream::failure when i/o error
-	std::vector<NormalizedArticle> read(std::string const& filePath) const;
+	std::vector<NormalizedArticle> readAndNormalizeArticles(std::string const& filePath) const;
+	std::vector<std::string> readAndNormalizeText(std::string const& filePath) const;
 private:
 
 	NormalizedArticle createNormalizedArticle(std::string const& title, std::string const& content) const;
 
-	Lemmatizer _lemmatizer;
+	Normalizer _normalizer;
+	XmlSourceParser _parser;
 };
